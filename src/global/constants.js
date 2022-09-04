4638fe27eb5bc53efc021757afd92974774bb332
timtsai <theblazertim1020@gmail.com>
2022-09-04 17:27:25 +0800

    [docs]Setup React frontend

---
+++ src/global/constants.js
@@ -0,0 +1,5 @@
+const SERVER_URL = 'http://localhost:3000';
+const SOCKET_URL = 'ws://localhost:3000';
+
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default { SERVER_URL, SOCKET_URL };
